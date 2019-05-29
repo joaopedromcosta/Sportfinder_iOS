fd6c84901c4ed4fda06e160d30aeae919df81e9b
joaocosta <joao.pedro.mc96@gmail.com>
2019-05-29 16:39:02 +0100

    Initial Commit

---
+++ Sportfinder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Sportfinder
+//
+//  Created by João Costa on 29/05/2019.
+//  Copyright © 2019 ipvc.estg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
