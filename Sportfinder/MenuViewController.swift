a3dc8c405d032f818caa08c37552b0776056d202
joaocosta <joao.pedro.mc96@gmail.com>
2019-05-29 23:31:22 +0100

    Storyboards for main menu buttons created

---
+++ Sportfinder/MenuViewController.swift
@@ -0,0 +1,25 @@
+//
+//  MenuViewController.swift
+//  Sportfinder
+//
+//  Created by João Costa on 29/05/2019.
+//  Copyright © 2019 ipvc.estg. All rights reserved.
+//
+
+
+import UIKit
+
+class MenuViewController: UIViewController {
+   
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    //Button add image and text
+    func btnPesquisarSetup(){
+        
+    }
+    
+}
+
---
+++ Sportfinder/SelecionarDesportoViewController.swift
@@ -0,0 +1,21 @@
+//
+//  MenuViewController.swift
+//  Sportfinder
+//
+//  Created by João Costa on 29/05/2019.
+//  Copyright © 2019 ipvc.estg. All rights reserved.
+//
+
+
+import UIKit
+
+class SelecionarDesportoViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+}
+
